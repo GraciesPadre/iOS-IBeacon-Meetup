60aec6d2e4bc0dd9410b38ae13c4994795050f86
John Dumais <doomer@johns-macbook-pro.local>
2016-01-06 20:29:08 -0700

    Initial Commit

---
+++ iOS IBeacon Meetup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS IBeacon Meetup
+//
+//  Created by John Dumais on 1/6/16.
+//  Copyright © 2016 John Dumais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
